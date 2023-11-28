51213d84d05e3ca870993582f2469da5d59f691b
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2023-11-28 23:45:19 +0300

    new дизайн main page

--- Prac2/JS/moreScript.js
+++ Prac2/JS/moreScript.js
@@ -1,68 +1,137 @@
-var queueMusic = ["деньги меняют", "Картины", "ну где моя нога?", "мой сларк", "Я поднимаю свою голову вверх", "как же он силён", "Качалка", "А ты?", "Хейтеры", "Вайбмен"];
-var queueAuthor = ["Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират"];
+var imgPath = "../media/";
+
+var queueMusicTop = ["Картины", "ну где моя нога?", "мой сларк", "Качалка", "А ты?", "Хейтеры", "Вайбмен"];
+var queueAuthorTop = ["Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират"];
+var queueImageTop = ["picture.jpg", "whereismyleg.jpg", "mySlark.jpg", "gym.jpg", "andYoujpg.jpg", "haters.jpg", "vibeman.jpg"];
+
+var btnRefreshTop = document.getElementById("btnRefreshTop");
+btnRefreshTop.style.display = "none";
+
+var listTop = document.getElementById("moreSectionTop");
 
 function displayMusicTop() {
 
-    let list = document.getElementById("moreSectionTop");
-    let itemDiv = document.createElement("div");
-    itemDiv.style.width = "500px";
-    itemDiv.style.transform = "skew(7deg)";
-    itemDiv.style.border = "1px solid";
-    itemDiv.style.background = "white";
-    itemDiv.style.margin = "2px";
-    itemDiv.style.borderRadius = "5px";
-    itemDiv.style.margin = "5px";
+    for (let i = 0; i < 3; i++) {
 
-    let name = String(queueMusic.shift());
-    let author = String(queueAuthor.shift());
+        let area = document.getElementById("areaToImageAndTextTop");
+        area.className = "areaToImageAndText";
 
-    queueMusic.push(name);
-    queueAuthor.push(author);
-    itemDiv.textContent = "🎵" + name + "\n" + "👤" + author;
-    itemDiv.style.whiteSpace = "pre-line";
-    list.appendChild(itemDiv);
+        let itemDivText = document.createElement("div");
+        itemDivText.className = "moreSectionText";
 
-    let btnRefresh = document.getElementById("btnRefreshTop");
-    btnRefresh.style.display = "block";
+        let itemDivImage = document.createElement("div");
+        itemDivImage.className = "moreSectionImage";
+
+        let name = String(queueMusicTop.shift());
+        let author = String(queueAuthorTop.shift());
+        let imagePath = imgPath + String(queueImageTop.shift());
+
+        queueMusicTop.push(name);
+        queueAuthorTop.push(author);
+        queueImageTop.push(imagePath);
+
+
+        let img = document.createElement('img');
+        img.src = imagePath;
+
+        let textName = document.createElement("text");
+        textName.textContent = "🎵" + name;
+        textName.style.color = "white";
+
+        let textAuthor = document.createElement("text");
+        textAuthor.textContent = "👤" + author;
+        textAuthor.style.color = "silver";
+
+        itemDivImage.appendChild(img);
+        itemDivText.appendChild(textName)
+        itemDivText.appendChild(textAuthor)
+
+        area.appendChild(itemDivImage);
+        area.appendChild(itemDivText);
+
+        listTop.appendChild(area);
+
+    }
+    btnRefreshTop.style.display = "block";
+
 }
 
 function refreshMusicTop() {
-    let list = document.getElementById("moreSectionTop");
-    list.innerHTML = "";
 
-    let btnRefresh = document.getElementById("btnRefreshTop");
-    btnRefresh.style.display = "none";
+    for (let area of listTop.childNodes){
+        while (area.firstChild){
+            area.removeChild(area.firstChild)
+        }
+    }
+
+    btnRefreshTop.style.display = "none";
 }
+
+
+
+var queueMusicBot = ["Маша", "Мой байк", "Ну и что, что я вор?", "Извини, сегодня праздник", "На луне", "Зомби апокалипсис", "where is my mind"];
+var queueAuthorBot = ["Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират", "Серега пират"];
+var queueImageBot = ["masha.jpg", "myBike.jpg", "vor.jpg", "sorryjpg.jpg", "luna.jpg", "zombie.jpg", "where is my mind.jpg"];
+
+var btnRefreshBot = document.getElementById("btnRefreshBot");
+btnRefreshBot.style.display = "none";
+
+var listBot = document.getElementById("moreSectionBot");
 
 function displayMusicBot() {
 
-    let list = document.getElementById("moreSectionBot");
-    let itemDiv = document.createElement("div");
-    itemDiv.style.width = "500px";
-    itemDiv.style.transform = "skew(7deg)";
-    itemDiv.style.border = "1px solid";
-    itemDiv.style.background = "white";
-    itemDiv.style.margin = "2px";
-    itemDiv.style.borderRadius = "5px";
+    for (let i = 0; i < 3; i++) {
+
+        let area = document.getElementById("areaToImageAndTextBot");
+        area.className = "areaToImageAndText";
+
+        let itemDivText = document.createElement("div");
+        itemDivText.className = "moreSectionText";
+
+        let itemDivImage = document.createElement("div");
+        itemDivImage.className = "moreSectionImage";
+
+        let name = String(queueMusicBot.shift());
+        let author = String(queueAuthorBot.shift());
+        let imagePath = imgPath + String(queueImageBot.shift());
+
+        queueMusicBot.push(name);
+        queueAuthorBot.push(author);
+        queueImageBot.push(imagePath);
 
 
-    let name = String(queueMusic.shift());
-    let author = String(queueAuthor.shift());
+        let img = document.createElement('img');
+        img.src = imagePath;
 
-    queueMusic.push(name);
-    queueAuthor.push(author);
-    itemDiv.textContent = "🎵" + name + "\n" + "👤" + author;
-    itemDiv.style.whiteSpace = "pre-line";
-    list.appendChild(itemDiv);
+        let textName = document.createElement("text");
+        textName.textContent = "🎵" + name;
+        textName.style.color = "white";
 
-    let btnRefresh = document.getElementById("btnRefreshBot");
-    btnRefresh.style.display = "block";
+        let textAuthor = document.createElement("text");
+        textAuthor.textContent = "👤" + author;
+        textAuthor.style.color = "silver";
+
+        itemDivImage.appendChild(img);
+        itemDivText.appendChild(textName)
+        itemDivText.appendChild(textAuthor)
+
+        area.appendChild(itemDivImage);
+        area.appendChild(itemDivText);
+
+        listBot.appendChild(area);
+
+    }
+    btnRefreshBot.style.display = "block";
+
 }
 
 function refreshMusicBot() {
-    let list = document.getElementById("moreSectionBot");
-    list.innerHTML = "";
 
-    let btnRefresh = document.getElementById("btnRefreshBot");
-    btnRefresh.style.display = "none";
+    for (let area of listBot.childNodes){
+        while (area.firstChild){
+            area.removeChild(area.firstChild)
+        }
+    }
+
+    btnRefreshBot.style.display = "none";
 }
