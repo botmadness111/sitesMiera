7b7163479316b9ad060e71faaa6dcafaba308655
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2023-12-04 22:26:08 +0300

    site

--- registration/JS/main.js
+++ registration/JS/main.js
@@ -2,10 +2,10 @@ function doRegistration() {
     if (document.getElementById("registrationInputPassword").value != "" && document.getElementById("registrationInputLogin").value != "") {
         document.getElementById("registrationInputPassword").value = "";
         document.getElementById("registrationInputLogin").value = "";
-        window.location.href = "../../main/Html/index.html";
+        window.location.href = "../../index.html";
     }
 }
 
 function doRegistrationGoogle() {
-    window.location.href = "../../main/Html/index.html";
+    window.location.href = "../../index.html";
 }
